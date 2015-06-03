29a286ae4d35df68a93c1edd4a50e79013a5d343
Christoph Schiessl <cs@proactive.cc>
2015-06-03 10:36:15 +0200

    remove stuff specific to the customer store

---
+++ spec/requests/get_ping_spec.rb
@@ -0,0 +1,13 @@
+require_relative "../spec_helper"
+require_relative "../../app/service"
+
+describe 'GET /ping' do
+  def app
+    Service
+  end
+
+  it "responds with status 200" do
+    get '/ping'
+    expect(last_response).to eq(200)
+  end
+end
