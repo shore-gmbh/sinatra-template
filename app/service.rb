4e3b7e6ebe48a31490773ebc5453c131c649d10b
Christoph Schiessl <cs@proactive.cc>
2015-06-03 11:21:18 +0200

    remove config for public folder -- we don't have a public folder

--- app/service.rb
+++ app/service.rb
@@ -2,9 +2,6 @@ require_relative '../config/boot'
 require 'sinatra/base'
 
 class Service < Sinatra::Base
-
-  set :public_folder => "public", :static => true
-
   get '/ping', provides: :json do
     [200, '{}']
   end
