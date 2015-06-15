433a36ef9a3966b9126c354761c2d84d35b31b46
Rudolf Schmidt <me@rudionrails.com>
2015-06-15 21:16:57 +0200

    Adds initial service documentation

--- app/service.rb
+++ app/service.rb
@@ -3,7 +3,43 @@ require 'sinatra/base'
 
 # Add your documentation for the HTTP service here.
 class Service < Sinatra::Base
+  ##
+  # GET /ping - Return serice ping
+  #
+  # ## Response
+  #
+  #   {}
+  #
   get '/ping', provides: :json do
     [200, '{}']
+  end
+
+  ##
+  # GET /v1/:oid/documents - Return all documents
+  #
+  # ## Request parameters
+  #
+  #   oid [String] The organization id
+  #
+  # ## Response Code
+  #
+  #   200
+  #
+  # ## Response Body
+  #
+  #   {
+  #     meta: {
+  #       current_page: 1,
+  #       total_pages: 3,
+  #       per_page: 25
+  #     },
+  #
+  #     documents: [
+  #       { ... },
+  #       { ... }
+  #     ]
+  #   }
+  get '/v1/:oid/documents' do
+    # ... add implementation here ...
   end
 end
