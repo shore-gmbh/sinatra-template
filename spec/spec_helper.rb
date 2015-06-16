cb5360ca294310c3a92537c00c0e466f1f8ab5dc
Christoph Schiessl <cs@proactive.cc>
2015-06-16 12:17:23 +0200

    format rspec output with the doc formatter if there's only a single spec file to run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,6 +39,15 @@ RSpec.configure do |config|
   #   - http://myronmars.to/n/dev-blog/2014/05/notable-changes-in-rspec-3#new__config_option_to_disable_rspeccore_monkey_patching
   config.disable_monkey_patching!
 
+  # Many RSpec users commonly either run the entire suite or an individual file,
+  # and it's useful to allow more verbose output when running an individual spec
+  # file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output, unless a formatter
+    # has already been configured (e.g. via a command-line flag).
+    config.default_formatter = 'doc'
+  end
+
   # Print the 5 slowest examples and example groups at the end of the spec run,
   # to help surface which specs are running particularly slow.
   config.profile_examples = 5
